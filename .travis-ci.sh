183e4f339ee8648ee70a993357d210f4ad663e7b
Karl Palmskog <palmskog@gmail.com>
2017-01-15 14:36:45 -0600

    use opam-dev repo in travis

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,15 +1,22 @@
+set -ev
+
 opam init --yes --no-setup
 eval $(opam config env)
+
 opam repo add coq-released https://coq.inria.fr/opam/released
-opam repo add distributedcomponents http://opam.distributedcomponents.net
-opam install coq.$COQ_VERSION coq-mathcomp-ssreflect.$SSREFLECT_VERSION \
-  StructTact InfSeqExt verdi verdi-runtime ounit.2.0.0 --yes --verbose
+opam repo add distributedcomponents-dev http://opam-dev.distributedcomponents.net
+
+opam pin add coq $COQ_VERSION --yes --verbose
+opam pin add coq-mathcomp-ssreflect $SSREFLECT_VERSION --yes --verbose
+opam install StructTact InfSeqExt verdi --yes --verbose
 
 case $MODE in
   chord)
+    opam install verdi-runtime --yes --verbose
     ./build.sh chord
     ;;
   chordshed)
+    opam install verdi-runtime --yes --verbose
     ./build.sh chordshed
     ;;
   *)
