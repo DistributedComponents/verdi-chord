9b7535233b30abb079cd737c1333cf2a3fe3fc60
Ryan Doenges <me@ryandoeng.es>
2017-03-01 14:50:52 -0800

    state zave's intermediate arguments

--- systems/ChordCorrectness.v
+++ systems/ChordCorrectness.v
@@ -6,12 +6,16 @@ Import ChordIDSpace.
 Require Import Chord.ChordProof.
 Require Import Chord.ChordSemantics.
 Import ChordSemantics.
+Import ConstrainedChord.
 
 Definition live (gst : global_state) (p : pointer) :=
   In (addr_of p) (nodes gst) /\
   ~ In (addr_of p) (failed_nodes gst).
 
-Definition correct_succs (gst : global_state) (h : pointer) (succs : list pointer) : Prop :=
+Definition correct_succs
+           (gst : global_state)
+           (h : pointer)
+           (succs : list pointer) : Prop :=
   forall s xs ys s',
     succs = xs ++ s :: ys ->
     ptr_between h s' s ->
@@ -21,12 +25,49 @@ Definition correct_succs (gst : global_state) (h : pointer) (succs : list pointe
 Definition full_succs (gst : global_state) (h : pointer) (succs : list pointer) : Prop :=
   length succs = Chord.SUCC_LIST_LEN.
 
+Print best_succ.
+
+(* cf. zave page 11 *)
+Definition first_succ_is_best_succ (gst : global_state) (h : addr) :=
+  exists st s rest,
+    sigma gst h = Some st /\
+    succ_list st = s :: rest /\
+    best_succ gst h (addr_of s).
+
+Definition all_first_succs_best (gst : global_state) :=
+  forall h,
+    live gst h ->
+    first_succ_is_best_succ gst (addr_of h).
+
+Definition eventually_always_net
+           (P : global_state -> Prop) : infseq.infseq occurrence -> Prop :=
+  infseq.eventually
+    (infseq.always
+       (fun ex' => P (occ_gst (infseq.hd ex')))).
+
+Definition phase_one (ex : infseq.infseq occurrence) :=
+  lb_execution ex ->
+  reachable_st (occ_gst (infseq.hd ex)) ->
+  eventually_always_net all_first_succs_best ex.
+ 
+
 Definition correct_pred (gst : global_state) (h : pointer) (p : option pointer) : Prop :=
   exists p0,
     p = Some p0 /\
     forall p',
       live gst p' ->
       ~ ptr_between p0 p' h.
+
+Definition correct_pointers gst :=
+  forall h st,
+    correct_pred gst h (pred st) /\
+    full_succs gst h (succ_list st) /\
+    correct_succs gst h (succ_list st).
+ 
+Definition phase_two (ex : infseq.infseq occurrence) :=
+  lb_execution ex ->
+  reachable_st (occ_gst (infseq.hd ex)) ->
+  eventually_always_net correct_pointers ex.
 
 Definition ideal (gst : global_state) : Prop :=
   forall h st,
@@ -50,3 +91,4 @@ Theorem chord_stabilization :
          (fun ex' => (ideal (occ_gst (infseq.hd ex')))))
       ex.
 Admitted.
+
