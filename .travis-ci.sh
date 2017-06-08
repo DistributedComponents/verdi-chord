6fb427dc7ab97ea162aedab6f953f8ee80430bae
Karl Palmskog <palmskog@gmail.com>
2017-06-08 17:52:57 -0500

    work around problem that mkreport.sh requires git repo

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -10,7 +10,10 @@ opam pin add coq $COQ_VERSION --yes --verbose
 
 case $MODE in
   proofalytics)
-    opam pin add verdi-chord-proofalytics . --yes --verbose &
+    opam pin add verdi-chord . --yes --verbose --no-action
+    opam install verdi-chord --yes --verbose --deps-only
+    ./configure
+    make proofalytics &
     # Output to the screen every 9 minutes to prevent a travis timeout
     export PID=$!
     while [[ `ps -p $PID | tail -n +2` ]]; do
