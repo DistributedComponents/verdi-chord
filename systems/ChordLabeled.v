19dab012e3ab8f992f46fd80fed91c654ed47ece
Doug Woos <doug.woos@gmail.com>
2017-06-22 16:21:27 -0700

    awful proof that i feel pretty bad about

--- systems/ChordLabeled.v
+++ systems/ChordLabeled.v
@@ -1132,13 +1132,39 @@ Lemma active_node_eventually_joins :
 Proof.
 Admitted.
 
+
+Ltac live_node_invariant_finish_goal :=
+  unfold live_node in *; simpl in *; intuition;
+  update_destruct; subst; rewrite_update; eauto;
+  break_exists; repeat find_inversion; eexists; intuition; eauto; simpl in *; congruence.
+
 Lemma live_node_invariant :
   forall gst l gst' h,
     labeled_step_dynamic gst l gst' ->
     live_node gst h ->
     live_node gst' h.
 Proof.
-Admitted.
+  intros.
+  inv_labeled_step.
+  - unfold timeout_handler_l, timeout_handler_eff,
+    tick_handler, keepalive_handler, do_rectify, request_timeout_handler,
+    add_tick, handle_query_timeout, clear_query, end_query, start_query,
+    update_query, update_succ_list in *.
+    repeat break_match; live_node_invariant_finish_goal.
+  - repeat unfold recv_handler_l, recv_handler,
+    handle_msg, do_delayed_queries,
+    clear_delayed_queries, tick_handler, keepalive_handler, do_rectify, request_timeout_handler,
+    handle_rectify, handle_query_req,
+    handle_query_req_busy, handle_query_res, start_query,
+    schedule_rectify_with,
+    handle_stabilize,
+    add_tick,
+    update_query, update_succ_list,
+    end_query, clear_query
+      in *. repeat break_match; live_node_invariant_finish_goal.
+  - live_node_invariant_finish_goal.
+  - live_node_invariant_finish_goal.
+Qed.
 
 Lemma enabled_Tick_invariant :
   forall gst l gst' eff h,
