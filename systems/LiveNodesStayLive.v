99697499bb005928b2e03a1539366acaf8fd63c9
Doug Woos <doug.woos@gmail.com>
2017-06-22 16:57:38 -0700

    move awful proof to separate file so ChordLabeled is maintianable

---
+++ systems/LiveNodesStayLive.v
@@ -0,0 +1,63 @@
+Require Import StructTact.StructTactics.
+Require Import StructTact.Util.
+Require Import Verdi.DynamicNet.
+Require Chord.Chord.
+Import Chord.Chord.Chord.
+Import Chord.ChordIDSpace.
+Require Import Chord.ChordLocalProps.
+Require Import Chord.ChordProof.
+Require Import Chord.ChordDefinitionLemmas.
+Require Import List.
+Import ListNotations.
+(*Require Import Wf_nat.*)
+Require Import InfSeqExt.infseq.
+Require Import InfSeqExt.classical.
+Require Import mathcomp.ssreflect.ssreflect.
+Require Import mathcomp.ssreflect.ssrbool.
+
+Set Bullet Behavior "Strict Subproofs".
+
+Require Chord.ChordSemantics.
+Import Chord.ChordSemantics.ChordSemantics.
+Import Chord.ChordSemantics.ConstrainedChord.
+Require Import Chord.ChordDefinitionLemmas.
+Require Import Chord.Measure.
+Require Import Chord.InfSeqTactics.
+
+
+Ltac live_node_invariant_finish_goal :=
+  unfold live_node in *; simpl in *; intuition;
+  update_destruct; subst; rewrite_update; eauto;
+  break_exists; repeat find_inversion; eexists; intuition; eauto; simpl in *; congruence.
+
+Lemma live_node_invariant :
+  forall gst l gst' h,
+    labeled_step_dynamic gst l gst' ->
+    live_node gst h ->
+    live_node gst' h.
+Proof.
+  intros.
+  match goal with
+  | H : labeled_step_dynamic _ _ _ |- _ =>
+    inv H
+  end.
+  - unfold timeout_handler_l, timeout_handler_eff,
+    tick_handler, keepalive_handler, do_rectify, request_timeout_handler,
+    add_tick, handle_query_timeout, clear_query, end_query, start_query,
+    update_query, update_succ_list in *.
+    repeat break_match; live_node_invariant_finish_goal.
+  - repeat unfold recv_handler_l, recv_handler,
+    handle_msg, do_delayed_queries,
+    clear_delayed_queries, tick_handler, keepalive_handler, do_rectify, request_timeout_handler,
+    handle_rectify, handle_query_req,
+    handle_query_req_busy, handle_query_res, start_query,
+    schedule_rectify_with,
+    handle_stabilize,
+    add_tick,
+    update_query, update_succ_list,
+    end_query, clear_query
+      in *. repeat break_match; live_node_invariant_finish_goal.
+  - live_node_invariant_finish_goal.
+  - live_node_invariant_finish_goal.
+Qed.
+
