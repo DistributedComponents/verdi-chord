1730c1adfe90cda47749067f6f03210fde9a0a0d
Doug Woos <doug.woos@gmail.com>
2017-10-10 13:54:41 -0700

    add some invariants about joining

--- systems/chord-props/NodesNotJoinedHaveNoSuccessors.v
+++ systems/chord-props/NodesNotJoinedHaveNoSuccessors.v
@@ -5,6 +5,24 @@ Require Import Chord.Chord.
 
 Require Import Chord.SystemReachable.
 
+Theorem cur_request_join_not_joined :
+  forall gst,
+    reachable_st gst ->
+    forall h st p q m,
+      sigma gst h = Some st ->
+      cur_request st = Some (p, Join q, m) ->
+      joined st = false.
+Admitted.
+
+Theorem cur_request_join2_not_joined :
+  forall gst,
+    reachable_st gst ->
+    forall h st p q m,
+      sigma gst h = Some st ->
+      cur_request st = Some (p, Join2 q, m) ->
+      joined st = false.
+Admitted.
+
 Theorem nodes_not_joined_have_no_successors :
   forall gst h st,
     reachable_st gst ->
