1597f0f2124d15a01dd5e7f2341f8cbe6a621b68
Ryan Doenges <me@ryandoeng.es>
2017-10-11 19:32:46 -0400

    prove NodesHaveState with chord_net_invariant

--- systems/chord-props/NodesHaveState.v
+++ systems/chord-props/NodesHaveState.v
@@ -12,41 +12,28 @@ Require Import Chord.LabeledLemmas.
 
 Set Bullet Behavior "Strict Subproofs".
 
+Definition nodes_have_state_invariant (gst : global_state) :=
+  forall h, In h (nodes gst) ->
+       exists st,
+         sigma gst h = Some st.
+Hint Unfold nodes_have_state_invariant.
+
 Theorem nodes_have_state_preserved :
-  forall gst gst',
-    (forall h, In h (nodes gst) -> exists st, sigma gst h = Some st) ->
-    step_dynamic gst gst' ->
-    (forall h, In h (nodes gst') -> exists st, sigma gst' h = Some st).
+  forall gst,
+    reachable_st gst ->
+    nodes_have_state_invariant gst.
 Proof using.
-  intros.
-  break_step.
-  - destruct (addr_eq_dec h h0).
-    + subst_max.
-      apply update_for_start_sigma_h_exists.
-    + find_rewrite_lem update_for_start_nodes_eq.
-      find_apply_lem_hyp in_inv.
-      break_or_hyp; try (exfalso; eauto; fail).
-      find_apply_hyp_hyp.
-      break_exists_exists.
-      eapply update_for_start_sigma_h_n; eauto.
-  - eauto.
-  - destruct (addr_eq_dec h h0).
-    * eexists.
-      now apply update_eq.
-    * apply_prop_hyp In (In h).
-      break_exists_exists.
-      now eauto using sigma_ahr_passthrough.
-  - destruct (addr_eq_dec (fst (snd m)) h).
-    * eexists.
-      now apply update_eq.
-    * simpl.
-      find_apply_hyp_hyp.
-      break_exists_exists.
-      repeat find_reverse_rewrite.
-      now apply update_diff.
-  - admit.
-  - admit.
-Admitted.
+  eapply chord_net_invariant; autounfold; intros;
+    repeat find_rewrite;
+    repeat (update_destruct; rewrite_update);
+      eauto with datatypes.
+  - unfold initial_st in *; break_and.
+    destruct (start_handler h (nodes gst)) as [[? ?] ?] eqn:?.
+    eapply_prop_hyp start_handler start_handler; auto.
+    break_and; eauto.
+  - simpl in *; break_or_hyp;
+      congruence || eauto.
+Qed.
 
 Lemma nodes_have_state :
   forall gst h,
@@ -56,11 +43,5 @@ Lemma nodes_have_state :
       sigma gst h = Some st.
 Proof.
   intros.
-  generalize dependent h.
-  induct_reachable_st; intros.
-  - unfold initial_st in *; break_and.
-    destruct (start_handler h (nodes gst)) as [[? ?] ?] eqn:?.
-    eapply_prop_hyp start_handler start_handler; auto.
-    break_and; eauto.
-  - eapply nodes_have_state_preserved; eauto.
+  now eapply nodes_have_state_preserved.
 Qed.
