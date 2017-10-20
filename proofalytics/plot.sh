8e20f7cf51a491b5efaa05c56475802fd776c4a1
Zach Tatlock <ztatlock@cs.washington.edu>
2017-10-20 13:54:00 -0700

    PA: massaging gnuplot script

--- proofalytics/plot.sh
+++ proofalytics/plot.sh
@@ -4,29 +4,32 @@ name="$(basename "$1" .csv)"
 tmp="$(mktemp "pa-plot-$name.XXXXXX")"
 
 cat "$1" \
-  | sed 's/PA-\([^-]*\)-[^,]*,\(.*\)/\1 \2/g' \
+  | sed 's/PA-\([^-]*\)-[^,]*,\s*\(.*\)/\1 \2/g' \
   | sort \
   | uniq \
   | grep -v '[a-z]' \
   > "$tmp"
 
 gnuplot <<EOF
-set title "$name"
-set timefmt "%y%m%d"
-set xdata time
 set terminal pngcairo dashed
 set output "$name.png"
-unset label
-set xtics rotate
+
+set title "$name"
+set xdata time
+set timefmt "%y%m%d"
+set format x "%b %d"
+set xtics rotate by 45 right
 set offset graph 0.01, graph 0.01, graph 0.05, graph 0.05
-plot "$tmp" using 1:2 notitle with points \
+plot "$tmp" using 1:2 with points \
         pointtype 5 \
-        linecolor rgb "#00aa00", \
-     "$tmp" using 1:2 notitle smooth csplines with lines \
+        linecolor rgb "#00aa00" \
+        notitle, \
+     "$tmp" using 1:2 smooth csplines with lines \
         linetype 2 \
-        dashtype "-.-" \
+        linewidth 2 \
         linecolor rgb "#888888" \
-        linewidth 2
+        dashtype "-" \
+        notitle
 EOF
 
 if [ $? -ne 0 ]; then
