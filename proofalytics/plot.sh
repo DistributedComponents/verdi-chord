b79bc942a08f0555eca89aae21fc28d30cc81d9c
Zach Tatlock <ztatlock@cs.washington.edu>
2017-10-20 07:43:14 -0700

    wip debugging gnuplot for PA

--- proofalytics/plot.sh
+++ proofalytics/plot.sh
@@ -1,26 +1,36 @@
 #!/usr/bin/env bash
 
-set -e
-
-name=$(basename "$1" .csv)
+name="$(basename "$1" .csv)"
+tmp="$(mktemp "pa-plot-$name.XXXXXX")"
 
 cat "$1" \
   | tail -n +1 \
   | sed 's/PA-\([^-]*\)-[^,]*,\(.*\)/\1 \2/g' \
   | sort \
   | uniq \
-  > pa-plots-tmp.csv
+  > "$tmp"
 
 gnuplot <<EOF
 set title "$name"
 set timefmt "%y%m%d"
 set xdata time
-set terminal png
+set terminal pngcairo dashed
 set output "$name.png"
 unset label
 set offset graph 0.01, graph 0.01, graph 0.05, graph 0.05
-plot "pa-plots-tmp.csv" using 1:2 notitle, \
-     "pa-plots-tmp.csv" using 1:2 notitle smooth csplines
+plot "$tmp" using 1:2 notitle \
+        with points pt 5 lc rgb "#00aa00", \
+     "$tmp" using 1:2 notitle smooth csplines \
+        with lines lt 1 dt "-.-" lc rgb "#888888" lw 2
 EOF
 
-rm pa-plots-tmp.csv
+if [ $? -ne 0 ]; then
+  echo "Failed to plot. $1 contained:"
+  cat "$1"
+  echo
+  echo "Massaged input to gnuplot was:"
+  cat "$tmp"
+  echo
+fi
+
+rm "$tmp"
