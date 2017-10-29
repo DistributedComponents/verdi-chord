70115eb04196f5ba6213284d4455daea21147796
Doug Woos <doug.woos@gmail.com>
2017-10-28 23:16:47 -0700

    add stronger version of WfPtrSuccListInvariant

--- systems/chord-props/WfPtrSuccListInvariant.v
+++ systems/chord-props/WfPtrSuccListInvariant.v
@@ -1,17 +1,20 @@
 Require Import List.
 Import ListNotations.
 
+Require Import StructTact.StructTactics.
+Require Import StructTact.ListTactics.
+
 Require Import Chord.Chord.
 
 Require Import Chord.SystemLemmas.
 Require Import Chord.SystemReachable.
 Require Import Chord.SystemPointers.
 
-Lemma wf_ptr_succ_list_invariant :
-  forall gst h st p rest,
+Lemma wf_ptr_succ_list_invariant' :
+  forall gst h st p,
     reachable_st gst ->
     sigma gst h = Some st ->
-    succ_list st = p :: rest ->
+    In p (succ_list st) ->
     wf_ptr p.
 Proof.
 (*
@@ -23,3 +26,15 @@ DIFFICULTY: 3
 USED: In phase one.
 *)
 Admitted.
+
+
+Lemma wf_ptr_succ_list_invariant :
+  forall gst h st p rest,
+    reachable_st gst ->
+    sigma gst h = Some st ->
+    succ_list st = p :: rest ->
+    wf_ptr p.
+Proof.
+  intros.
+  eapply wf_ptr_succ_list_invariant'; eauto. find_rewrite. in_crush.
+Qed.
