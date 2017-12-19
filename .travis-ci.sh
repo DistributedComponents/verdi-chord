8b8287d63a36a9d74314cf04332ab515f759d728
Karl Palmskog <palmskog@gmail.com>
2017-12-18 23:43:07 -0600

    one more Travis caching adjustment

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -11,18 +11,18 @@ fi
 
 opam update --yes --verbose
 
-opam pin add coq $COQ_VERSION --yes
-opam install coq --yes
+opam pin add coq ${COQ_VERSION} --yes
+opam upgrade coq --yes
 
 opam upgrade --yes --verbose
 
-case $MODE in
+case ${MODE} in
   proofalytics)
     opam pin add verdi-chord . --yes --verbose --no-action
     opam install verdi-chord --yes --verbose --deps-only
     ./configure
     make proofalytics &
-    # Output to the screen every 4 minutes to prevent a travis timeout
+    # Output to the screen every few minutes to prevent a travis timeout
     export PID=$!
     while [[ `ps -p $PID | tail -n +2` ]]; do
 	echo 'proofalyzing...'
