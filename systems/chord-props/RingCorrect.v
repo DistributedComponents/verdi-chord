d97d614f10562514378b6e76670d0621fa6ed559
Ryan Doenges <me@ryandoeng.es>
2018-01-06 15:58:30 -0800

    little improvement in a recv case

--- systems/chord-props/RingCorrect.v
+++ systems/chord-props/RingCorrect.v
@@ -1089,31 +1089,31 @@ Proof.
         find_apply_lem_hyp handle_query_res_definition; expand_def;
           try congruence;
           try inv_prop request_payload;
-          try find_injection.
-        -- assert (Exists (live_node gst) (map addr_of (chop_succs ((make_pointer (addr_of x)) :: x13)))).
-           {
-             find_eapply_prop live_node_in_msg_succ_lists; eauto.
-             repeat find_rewrite; constructor; in_crush.
-             (* x is joined because we're stabilizing with it. *)
-             admit.
-           }
-           find_apply_lem_hyp Exists_exists; break_exists.
-           break_and.
-           assert (live_node gst' x0).
-           {
-             break_live_node.
-             destruct (addr_eq_dec x0 h).
-             - eapply live_node_characterization; repeat find_rewrite; rewrite_update; eauto.
-               find_apply_lem_hyp joined_preserved_by_do_delayed_queries.
-               find_apply_lem_hyp joined_preserved_by_handle_stabilize.
-               congruence.
-             - eapply live_node_characterization; repeat find_rewrite; rewrite_update; eauto.
-           }
-           (* we know there's a live node in succ_list x8, so there's got to be
-              a best_succ as well *)
-           unfold best_succ.
-           admit.
-        -- handler_def.
+          try find_injection;
+          try solve [handler_def].
+        assert (Exists (live_node gst) (map addr_of (chop_succs ((make_pointer (addr_of x)) :: x13)))).
+        {
+          find_eapply_prop live_node_in_msg_succ_lists; eauto.
+          repeat find_rewrite; constructor; in_crush.
+          (* x is joined because we're stabilizing with it. *)
+          admit.
+        }
+        find_apply_lem_hyp Exists_exists; break_exists.
+        break_and.
+        assert (live_node gst' x0).
+        {
+          break_live_node.
+          destruct (addr_eq_dec x0 h).
+          - eapply live_node_characterization; repeat find_rewrite; rewrite_update; eauto.
+            find_apply_lem_hyp joined_preserved_by_do_delayed_queries.
+            find_apply_lem_hyp joined_preserved_by_handle_stabilize.
+            congruence.
+          - eapply live_node_characterization; repeat find_rewrite; rewrite_update; eauto.
+        }
+        (* we know there's a live node in succ_list x8, so there's got to be
+           a best_succ as well *)
+        unfold best_succ.
+        admit.
       * handler_def.
         simpl in *; repeat find_rewrite.
         break_if; try congruence.
